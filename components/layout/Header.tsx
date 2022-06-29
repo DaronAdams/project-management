5d448dfcc340e07c2172e3a9a39c359129a46e4b
DaronAdams <70734883+DaronAdams@users.noreply.github.com>
2022-06-28 21:31:51 -0500

    Creater base header component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,1 +1,19 @@
 import React from "react";
+
+const Header = () => {
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">Project Manager</a>
+            </div>
+            <div className="flex-none">
+                <button className="btn btn-square btn-ghost">
+                Log in
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}",
   ],
   presets: [],
   darkMode: 'media', // or 'class'
@@ -952,5 +952,5 @@ module.exports = {
     'active',
     'disabled',
   ],
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"),require("daisyui")],
 }
